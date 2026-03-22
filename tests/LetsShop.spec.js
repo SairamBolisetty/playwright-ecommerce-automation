722303f4045b656fe57e2e310260dc8f1a4bfbc2
SairamBolisetty <Bolisetty.sairam01@gmail.com>
2026-03-22 14:43:40 +0530

    Added POManager

--- tests/LetsShop.spec.js
+++ tests/LetsShop.spec.js
@@ -1,22 +1,23 @@
 const {test,expect} = require('@playwright/test');
-const {LoginPage}= require('../pageobjects/LoginPage');
-const {HomePage}= require('../pageobjects/HomePage');
-const {CartPage}=require('../pageobjects/CartPage');
-const {PaymentPage}= require('../pageobjects/PaymentPage');
+const {POManager}= require('../pageobjects/PoManager');
+
+
 
 test(`@Login LetsShop test`, async ({page}) =>
 {
-    const loginPage= new LoginPage(page);
-    const homePage= new HomePage(page);
-    const cartPage= new CartPage(page);
-    const paymentPage= new PaymentPage(page);
+    const poManager= new POManager(page);
     const username= 'sairam75@gmail.com';
     const password= 'Sairam@123';
+    const loginPage= poManager.getLoginPage();
+    const homePage= poManager.getHomePage();
+    const cartPage= poManager.getCartPage();
+    const paymentPage= poManager.getPaymentPage();
+
     await loginPage.launch();
     await loginPage.ValidLogin(username,password);
     await homePage.getProductsName();
     await cartPage.checkoutclick();
     await paymentPage.checkOut();
-    await page.pause();
+    //await page.pause();
 
 })
